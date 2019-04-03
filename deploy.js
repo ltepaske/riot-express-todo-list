d56ca3b595342936c6ada5333d79a3adffd33847
Lauren <laurentepaske@gmail.com>
2019-04-03 11:44:47 -0700

    added 1 and changed yml

--- deploy.js
+++ deploy.js
@@ -22,8 +22,8 @@ function installPM2() {
 // transfers local project to the remote server
 function transferProjectToRemote(failed, successful) {
   return ssh.putDirectory(
-    '../deployment-handson-two',
-    '/home/ubuntu/deployment-handson-two-temp',
+    '../riot-express-todo-list',
+    '/home/ubuntu/riot-express-todo-list-temp',
     {
       recursive: true,
       concurrency: 1,
@@ -49,7 +49,7 @@ function transferProjectToRemote(failed, successful) {
 // creates a temporary folder on the remote server
 function createRemoteTempFolder() {
   return ssh.execCommand(
-    'rm -rf deployment-handson-two-temp && mkdir deployment-handson-two-temp', {
+    'rm -rf riot-express-todo-list-temp && mkdir riot-express-todo-list-temp', {
       cwd: '/home/ubuntu'
   });
 }
@@ -65,7 +65,7 @@ function stopRemoteServices() {
 // updates the project source on the server
 function updateRemoteApp() {
   return ssh.execCommand(
-    'cp -r deployment-handson-two-temp/* deployment-handson-two/ && rm -rf deployment-handson-two-temp', {
+    'cp -r riot-express-todo-list-temp/* riot-express-todo-list/ && rm -rf riot-express-todo-list-temp', {
       cwd: '/home/ubuntu'
   });
 }
@@ -73,7 +73,7 @@ function updateRemoteApp() {
 // restart mongodb and node services on the remote server
 function restartRemoteServices() {
   return ssh.execCommand(
-    'cd deployment-handson-two && sudo service mongod start && pm2 start app.js', {
+    'cd riot-express-todo-list && sudo service mongod start && pm2 start app.js', {
       cwd: '/home/ubuntu'
   });
 }
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '3.89.219.209',
+      host: '3.84.251.228',
       username: 'ubuntu',
       privateKey: 're-key-copy.pem'
     })
@@ -95,7 +95,7 @@ function sshConnect() {
       return installPM2();
     })
     .then(function() {
-      console.log('Creating `deployment-handson-two-temp` folder.');
+      console.log('Creating `riot-express-todo-list-temp` folder.');
       return createRemoteTempFolder();
     })
     .then(function(result) {
